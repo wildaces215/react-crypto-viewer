8f1a827eb5017d33a6e729d95c82d5897b31afbc
wildaces215 <wannbecoastie@gmail.com>
2019-09-12 18:55:32 -0400

    Added build directory

---
+++ build/precache-manifest.3ae98957cbbc4088961557142719b19d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275cdf44ab821755befa800b5c59b54d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "537013c812267d879b5f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1e973bcc29604aac4955",
+    "url": "/static/js/2.63b4eac2.chunk.js"
+  },
+  {
+    "revision": "537013c812267d879b5f",
+    "url": "/static/js/main.af6762f0.chunk.js"
+  },
+  {
+    "revision": "2973641d2dec920c5a02",
+    "url": "/static/js/runtime~main.b7d5c152.js"
+  }
+]);
