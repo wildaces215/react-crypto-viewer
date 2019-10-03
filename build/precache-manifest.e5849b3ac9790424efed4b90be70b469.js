2f56990f360dc4c7e3143951aeb67637a9d01ba1
wildaces215 <wannbecoastie@gmail.com>
2019-10-03 15:50:56 -0400

    Added bild

---
+++ build/precache-manifest.e5849b3ac9790424efed4b90be70b469.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c5d106dbececa674b70162a1b56606",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41acbc4e926b0d58b72",
+    "url": "/static/css/main.58f296f9.chunk.css"
+  },
+  {
+    "revision": "28f7b7ae258840f6c235",
+    "url": "/static/js/2.aed2d470.chunk.js"
+  },
+  {
+    "revision": "d41acbc4e926b0d58b72",
+    "url": "/static/js/main.41386f13.chunk.js"
+  },
+  {
+    "revision": "2973641d2dec920c5a02",
+    "url": "/static/js/runtime~main.b7d5c152.js"
+  }
+]);
